a2b5fe01983a691910a18ef93c3a4d03d9ea8ba7
Herklos <herklos.dev@protonmail.com>
2019-05-20 23:37:35 +0200

    Initial commit

---
+++ octobot_trading/enums.py
@@ -0,0 +1,177 @@
+#  Drakkar-Software OctoBot-Trading
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library
+from enum import Enum
+
+
+class TradeOrderSide(Enum):
+    BUY = "buy"
+    SELL = "sell"
+
+
+class TradeOrderType(Enum):
+    LIMIT = "limit"
+    MARKET = "market"
+
+
+class OrderStatus(Enum):
+    FILLED = "closed"
+    OPEN = "open"
+    PARTIALLY_FILLED = "partially_filled"
+    CANCELED = "canceled"
+    CLOSED = "closed"
+
+
+class TraderOrderType(Enum):
+    BUY_MARKET = 1
+    BUY_LIMIT = 2
+    STOP_LOSS = 3
+    STOP_LOSS_LIMIT = 4
+    SELL_MARKET = 5
+    SELL_LIMIT = 6
+    TRAILING_STOP = 7
+
+
+class ExchangeConstantsTickersColumns(Enum):
+    SYMBOL = "symbol"
+    TIMESTAMP = "timestamp"
+    DATETIME = "datetime"
+    HIGH = "high"
+    LOW = "low"
+    BID = "bid"
+    BID_VOLUME = "bidVolume"
+    ASK = "ask"
+    ASK_VOLUME = "askVolume"
+    VWAP = "vwap"
+    OPEN = "open"
+    CLOSE = "close"
+    LAST = "last"
+    PREVIOUS_CLOSE = "previousClose"
+    CHANGE = "change"
+    PERCENTAGE = "percentage"
+    AVERAGE = "average"
+    BASE_VOLUME = "baseVolume"
+    QUOTE_VOLUME = "quoteVolume"
+    INFO = "info"
+
+
+class ExchangeConstantsTickersInfoColumns(Enum):
+    SYMBOL = "symbol"
+    PRICE_CHANGE = "priceChange"
+    PRICE_CHANGE_PERCENT = "priceChangePercent"
+    WEIGHTED_AVERAGE_PRICE = "weightedAvgPrice"
+    PREVIOUS_CLOSE_PRICE = "prevClosePrice"
+    LAST_PRICE = "lastPrice"
+    LAST_QUANTITY = "lastQty"
+    BID_PRICE = "bidPrice"
+    BID_QUANTITY = "bidQty"
+    ASK_PRICE = "askPrice"
+    ASK_QUANTITY = "askQty"
+    OPEN_PRICE = "openPrice"
+    HIGH_PRICE = "highPrice"
+    LOW_PRICE = "lowPrice"
+    VOLUME = "volume"
+    QUOTE_VOLUME = "quoteVolume"
+    OPEN_TIME = "openTime"
+    CLOSE_TIME = "closeTime"
+    FIRST_ID = "firstId"
+    LAST_ID = "lastId"
+    COUNT = "count"
+
+
+class ExchangeConstantsMarketStatusColumns(Enum):
+    SYMBOL = "symbol"
+    ID = "id"
+    CURRENCY = "base"
+    MARKET = "quote"
+    ACTIVE = "active"
+    PRECISION = "precision"  # number of decimal digits "after the dot"
+    PRECISION_PRICE = "price"
+    PRECISION_AMOUNT = "amount"
+    PRECISION_COST = "cost"
+    LIMITS = "limits"  # value limits when placing orders on this market
+    LIMITS_AMOUNT = "amount"
+    LIMITS_AMOUNT_MIN = "min"  # order amount should be > min
+    LIMITS_AMOUNT_MAX = "max"  # order amount should be < max
+    LIMITS_PRICE = "price"  # same min/max limits for the price of the order
+    LIMITS_PRICE_MIN = "min"  # order price should be > min
+    LIMITS_PRICE_MAX = "max"  # order price should be < max
+    LIMITS_COST = "cost"  # same limits for order cost = price * amount
+    LIMITS_COST_MIN = "min"  # order cost should be > min
+    LIMITS_COST_MAX = "max"  # order cost should be < max
+    INFO = "info"
+
+
+class ExchangeConstantsMarketStatusInfoColumns(Enum):
+    # binance specific
+    FILTERS = "filters"
+    FILTER_TYPE = "filterType"
+    PRICE_FILTER = "PRICE_FILTER"
+    LOT_SIZE = "LOT_SIZE"
+    MIN_PRICE = "minPrice"
+    MAX_PRICE = "maxPrice"
+    TICK_SIZE = "tickSize"
+    MIN_QTY = "minQty"
+    MAX_QTY = "maxQty"
+
+
+class ExchangeConstantsOrderBookInfoColumns(Enum):
+    BIDS = "bids"
+    ASKS = "asks"
+    TIMESTAMP = "timestamp"
+    DATETIME = "datetime"
+    NONCE = "nonce"
+
+
+class ExchangeConstantsOrderColumns(Enum):
+    INFO = "info"
+    ID = "id"
+    TIMESTAMP = "timestamp"
+    DATETIME = 'datetime'
+    LAST_TRADE_TIMESTAMP = "lastTradeTimestamp"
+    SYMBOL = "symbol"
+    TYPE = "type"
+    SIDE = "side"
+    PRICE = "price"
+    AMOUNT = "amount"
+    COST = "cost"
+    AVERAGE = "average"
+    FILLED = "filled"
+    REMAINING = "remaining"
+    STATUS = "status"
+    FEE = "fee"
+    TRADES = "trades"
+    MAKER = "maker"
+    TAKER = "taker"
+
+
+class ExchangeConstantsFeesColumns(Enum):
+    TYPE = "type"
+    CURRENCY = "currency"
+    RATE = "rate"
+    COST = "cost"
+
+
+class ExchangeConstantsMarketPropertyColumns(Enum):
+    TAKER = "taker"  # trading
+    MAKER = "maker"  # trading
+    FEE = "fee"  # withdraw
+
+
+class FeePropertyColumns(Enum):
+    TYPE = "type"  # taker of maker
+    CURRENCY = "currency"  # currency the fee is paid in
+    RATE = "rate"  # multiplier applied to compute fee
+    COST = "cost"  # fee amount
---
+++ setup.py
@@ -0,0 +1,58 @@
+#  Drakkar-Software OctoBot-Commons
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+# from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from setuptools import find_packages
+from setuptools import setup, Extension
+
+from octobot_trading import PROJECT_NAME, VERSION
+
+ext_modules = []
+
+PACKAGES = find_packages(exclude=["tests"])
+
+# long description from README file
+with open('README.md', encoding='utf-8') as f:
+    DESCRIPTION = f.read()
+
+REQUIRED = open('requirements.txt').read()
+REQUIRES_PYTHON = '>=3.7'
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    url='https://github.com/Drakkar-Software/OctoBot-Trading',
+    license='LGPL-3.0',
+    author='Drakkar-Software',
+    author_email='drakkar-software@protonmail.com',
+    description='OctoBot project trading package',
+    packages=PACKAGES,
+    long_description=DESCRIPTION,
+    install_requires=REQUIRED,
+    cmdclass={'build_ext': build_ext},
+    tests_require=["pytest"],
+    test_suite="tests",
+    zip_safe=False,
+    data_files=[],
+    setup_requires=['Cython'],
+    python_requires=REQUIRES_PYTHON,
+    ext_modules=cythonize(ext_modules),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
