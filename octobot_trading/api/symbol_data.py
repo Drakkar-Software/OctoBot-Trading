6b3e5111274b0ecb4a84aa92d3a8edec38d00f0c
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-01-19 15:31:56 +0100

    add symbol data api methods

--- octobot_trading/api/symbol_data.py
+++ octobot_trading/api/symbol_data.py
@@ -14,6 +14,7 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 from octobot_trading.data_manager.candles_manager import CandlesManager
+from octobot_trading.data_manager.kline_manager import KlineManager
 from octobot_trading.exchanges.data.exchange_symbol_data import ExchangeSymbolData
 from octobot_commons.enums import TimeFrames
 
@@ -24,3 +25,11 @@ def get_symbol_data(exchange_manager, symbol) -> ExchangeSymbolData:
 
 def get_symbol_candles_manager(symbol_data, time_frame) -> CandlesManager:
     return symbol_data.symbol_candles[TimeFrames(time_frame)]
+
+
+def get_symbol_historical_candles(symbol_data, time_frame, limit=-1) -> dict:
+    return get_symbol_candles_manager(symbol_data, time_frame).get_symbol_prices(limit)
+
+
+def get_symbol_klines(symbol_data, time_frame) -> list:
+    return symbol_data.symbol_klines[TimeFrames(time_frame)].kline
