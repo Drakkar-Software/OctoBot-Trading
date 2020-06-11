2ccfe31c1582cb3f40c747fe44b0b48f7aefb071
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-06-12 00:12:11 +0200

    handle exchange trading type in exchange builder

--- octobot_trading/consumers/octobot_channel_consumer.py
+++ octobot_trading/consumers/octobot_channel_consumer.py
@@ -23,6 +23,8 @@ from octobot_trading.api.trader import is_trader_enabled_in_config, is_trader_si
 from octobot_trading.api.exchange import create_exchange_builder
 
 from octobot_commons.enums import OctoBotChannelSubjects
+from octobot_trading.constants import CONFIG_EXCHANGE_SANDBOXED, CONFIG_EXCHANGES, CONFIG_EXCHANGE_FUTURE, \
+    CONFIG_EXCHANGE_MARGIN, CONFIG_EXCHANGE_SPOT
 
 OCTOBOT_CHANNEL_TRADING_CONSUMER_LOGGER_TAG = "OctoBotChannelTradingConsumer"
 
@@ -68,15 +70,8 @@ async def _handle_creation(bot_id, action, data):
                                                        data[OctoBotChannelTradingDataKeys.EXCHANGE_NAME.value]) \
                 .has_matrix(data[OctoBotChannelTradingDataKeys.MATRIX_ID.value]) \
                 .use_tentacles_setup_config(data[OctoBotChannelTradingDataKeys.TENTACLES_SETUP_CONFIG.value]) \
-                .set_bot_id(bot_id) \
-                .is_rest_only()
-            if is_trader_enabled_in_config(config):
-                exchange_builder.is_real()
-            elif is_trader_simulator_enabled_in_config(config):
-                exchange_builder.is_simulated()
-            backtesting = data[OctoBotChannelTradingDataKeys.BACKTESTING.value]
-            if backtesting is not None:
-                exchange_builder.is_backtesting(backtesting)
+                .set_bot_id(bot_id)
+            _set_exchange_type_details(exchange_builder, config, data[OctoBotChannelTradingDataKeys.BACKTESTING.value])
             await exchange_builder.build()
             await get_chan_at_id(OctoBotChannelsName.OCTOBOT_CHANNEL.value, bot_id).get_internal_producer() \
                 .send(bot_id=bot_id,
@@ -85,3 +80,27 @@ async def _handle_creation(bot_id, action, data):
                       data={OctoBotChannelTradingDataKeys.EXCHANGE_ID.value: exchange_builder.exchange_manager.id})
         except Exception as e:
             get_logger(OCTOBOT_CHANNEL_TRADING_CONSUMER_LOGGER_TAG).error(f"Error when creating new exchange {e}")
+
+
+def _set_exchange_type_details(exchange_builder, config, backtesting):
+    # rest, web socket
+    exchange_builder.is_rest_only()
+    # real, simulator, backtesting
+    if is_trader_enabled_in_config(config):
+        exchange_builder.is_real()
+    elif is_trader_simulator_enabled_in_config(config):
+        exchange_builder.is_simulated()
+    if backtesting is not None:
+        exchange_builder.is_backtesting(backtesting)
+    # use exchange sandbox
+    exchange_builder.is_sandboxed(
+        config[CONFIG_EXCHANGES][exchange_builder.exchange_name].get(CONFIG_EXCHANGE_SANDBOXED, False)
+    )
+    # exchange trading type
+    if config[CONFIG_EXCHANGES][exchange_builder.exchange_name].get(CONFIG_EXCHANGE_FUTURE, False):
+        exchange_builder.is_future(True)
+    elif config[CONFIG_EXCHANGES][exchange_builder.exchange_name].get(CONFIG_EXCHANGE_MARGIN, False):
+        exchange_builder.is_margin(True)
+    elif config[CONFIG_EXCHANGES][exchange_builder.exchange_name].get(CONFIG_EXCHANGE_SPOT, True):
+        # Use spot trading as default trading type
+        exchange_builder.is_spot_only(True)
