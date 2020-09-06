510acf259dc1545ba88cdbdbe548979d26d81f4a
Herklos <herklos.dev@protonmail.com>
2020-09-06 16:30:35 +0200

    [Websocket] Add margin and future creation support

--- octobot_trading/exchanges/websockets/websockets_util.py
+++ octobot_trading/exchanges/websockets/websockets_util.py
@@ -18,22 +18,23 @@ from octobot_trading.exchanges.types.websocket_exchange import WebsocketExchange
 from octobot_trading.constants import CONFIG_EXCHANGES, CONFIG_EXCHANGE_WEB_SOCKET
 
 
-def force_disable_web_socket(config, exchange_name) -> bool:
+def force_disable_web_socket(config: dict, exchange_name: str) -> bool:
     return CONFIG_EXCHANGE_WEB_SOCKET in config[CONFIG_EXCHANGES][exchange_name] \
            and not config[CONFIG_EXCHANGES][exchange_name][CONFIG_EXCHANGE_WEB_SOCKET]
 
 
-def check_web_socket_config(config, exchange_name) -> bool:
+def check_web_socket_config(config: dict, exchange_name: str) -> bool:
     return not force_disable_web_socket(config, exchange_name)
 
 
-def search_websocket_class(websocket_class, exchange_name, tentacles_setup_config):
+def search_websocket_class(websocket_class, exchange_manager: object):
     for socket_manager in websocket_class.__subclasses__():
         # return websocket exchange if available
-        if socket_manager.has_name(exchange_name, tentacles_setup_config):
+        if socket_manager.has_name(exchange_manager):
             return socket_manager
     return None
 
 
-def get_exchange_websocket_from_name(name, tentacles_setup_config):
-    return get_class_from_name_with_activated_required_tentacles(name, WebsocketExchange, tentacles_setup_config)
+def get_exchange_websocket_from_name(name: str, tentacles_setup_config: object, with_class_method: str):
+    return get_class_from_name_with_activated_required_tentacles(name, WebsocketExchange,
+                                                                 tentacles_setup_config, with_class_method)
