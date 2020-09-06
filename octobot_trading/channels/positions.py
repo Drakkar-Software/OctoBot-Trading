60fcf8575be5dc28c56a3550540f749a6fd01fb8
Herklos <herklos.dev@protonmail.com>
2020-09-06 18:58:21 +0200

    [PositionUpdater] Drop None position

--- octobot_trading/channels/positions.py
+++ octobot_trading/channels/positions.py
@@ -27,6 +27,8 @@ class PositionsProducer(ExchangeChannelProducer):
     async def perform(self, positions, is_closed=False, is_liquidated=False, is_from_bot=True):
         try:
             for position in positions:
+                if not position:
+                    continue
                 symbol: str = self.channel.exchange_manager.get_exchange_symbol(
                     position[ExchangeConstantsPositionColumns.SYMBOL.value])
                 if self.channel.get_filtered_consumers(
