2b51d7450f0e3d42c8bf8461cf5318dd4b66844c
herklos <herklos.dev@protonmail.com>
2020-11-17 22:06:50 +0100

    [Exchanges] Refactor architecture
    Add connectors

--- octobot_trading/exchanges/__init__.pxd
+++ octobot_trading/exchanges/__init__.pxd
@@ -13,66 +13,54 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-from octobot_trading.exchanges cimport exchanges
 from octobot_trading.exchanges.exchanges cimport (
     ExchangeConfiguration,
     Exchanges,
 )
 
-from octobot_trading.exchanges cimport exchange_config_data
 from octobot_trading.exchanges.exchange_config_data cimport (
     ExchangeConfig,
 )
 
-from octobot_trading.exchanges cimport traders
 from octobot_trading.exchanges.traders cimport (
     Trader,
     TraderSimulator,
 )
 
-from octobot_trading.exchanges cimport abstract_exchange
 from octobot_trading.exchanges.abstract_exchange cimport (
     AbstractExchange,
 )
 
-from octobot_trading.exchanges cimport websockets
 from octobot_trading.exchanges.websockets cimport (
     AbstractWebsocket,
-    OctoBotWebSocketClient,
-    force_disable_web_socket,
+force_disable_web_socket,
     check_web_socket_config,
     search_websocket_class,
 )
 
-from octobot_trading.exchanges cimport exchange_manager
 from octobot_trading.exchanges.exchange_manager cimport (
     ExchangeManager,
 )
-from octobot_trading.exchanges cimport exchange_util
-from octobot_trading.exchanges.exchange_util cimport (
+from octobot_trading.exchanges.util.exchange_util cimport (
     get_margin_exchange_class,
     get_future_exchange_class,
     get_spot_exchange_class,
     get_order_side,
 )
-from octobot_trading.exchanges cimport util
 from octobot_trading.exchanges.util cimport (
     ExchangeMarketStatusFixer,
     is_ms_valid,
 )
-from octobot_trading.exchanges cimport types
 from octobot_trading.exchanges.types cimport (
     FutureExchange,
     WebsocketExchange,
     MarginExchange,
     SpotExchange,
 )
-from octobot_trading.exchanges cimport implementations
 from octobot_trading.exchanges.implementations cimport (
     ExchangeSimulator,
     CCXTExchange,
 )
-from octobot_trading.exchanges cimport exchange_builder
 from octobot_trading.exchanges.exchange_builder cimport (
     ExchangeBuilder,
 )
---
+++ octobot_trading/exchanges/connectors/__init__.py
@@ -0,0 +1,54 @@
+#  Drakkar-Software OctoBot-Trading
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+
+from octobot_trading.exchanges.implementations import exchange_simulator
+from octobot_trading.exchanges.implementations.exchange_simulator import (
+    ExchangeSimulator,
+)
+from octobot_trading.exchanges.implementations import spot_exchange_simulator
+from octobot_trading.exchanges.implementations.spot_exchange_simulator import (
+    SpotExchangeSimulator,
+)
+from octobot_trading.exchanges.implementations import future_exchange_simulator
+from octobot_trading.exchanges.implementations.future_exchange_simulator import (
+    FutureExchangeSimulator,
+)
+from octobot_trading.exchanges.implementations import margin_exchange_simulator
+from octobot_trading.exchanges.implementations.margin_exchange_simulator import (
+    MarginExchangeSimulator,
+)
+from octobot_trading.exchanges.implementations import ccxt_exchange
+from octobot_trading.exchanges.implementations.ccxt_exchange import (
+    CCXTExchange,
+)
+from octobot_trading.exchanges.implementations import spot_ccxt_exchange
+from octobot_trading.exchanges.implementations.spot_ccxt_exchange import (
+    SpotCCXTExchange,
+)
+from octobot_trading.exchanges.implementations import default_spot_ccxt
+from octobot_trading.exchanges.implementations.default_spot_ccxt import (
+    DefaultCCXTSpotExchange,
+)
+
+__all__ = [
+    "ExchangeSimulator",
+    "SpotExchangeSimulator",
+    "FutureExchangeSimulator",
+    "MarginExchangeSimulator",
+    "DefaultCCXTSpotExchange",
+    "CCXTExchange",
+    "SpotCCXTExchange",
+]
