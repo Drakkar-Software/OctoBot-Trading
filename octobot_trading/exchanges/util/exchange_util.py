f64577e53769a2b97b97c0dcbc1a7cf751f735b7
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-12-03 19:28:04 +0100

    use debug instead of warning on missing exchange implem

--- octobot_trading/exchanges/util/exchange_util.py
+++ octobot_trading/exchanges/util/exchange_util.py
@@ -55,8 +55,7 @@ def search_exchange_class_from_exchange_name(exchange_class, exchange_name,
     if enable_default:
         return None
 
-    logging.get_logger().warning(f"No specific exchange implementation for {exchange_name} "
-                                 f"found, searching for default one...")
+    logging.get_logger().debug(f"No specific exchange implementation for {exchange_name} found, using a default one.")
     return search_exchange_class_from_exchange_name(exchanges_types.SpotExchange, exchange_name,
                                                     tentacles_setup_config, enable_default=True)
 
