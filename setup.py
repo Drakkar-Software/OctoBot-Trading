646575dbc89882f3689353ec24ca54952e8a9b96
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-02-03 21:46:56 +0100

    bump requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import os
 
 from setuptools import dist
 
-dist.Distribution().fetch_build_eggs(['Cython>=0.29.21', 'numpy>=1.19.3'])
+dist.Distribution().fetch_build_eggs(['Cython>=0.29.21', 'numpy>=1.20.0'])
 
 import numpy as np
 
