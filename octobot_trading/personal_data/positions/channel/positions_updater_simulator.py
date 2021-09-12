e08d920baf8bb5cc07119a41f04e189cd7fb7e91
Herklos <herklos.dev@protonmail.com>
2021-09-12 15:40:14 +0200

    [PositionUpdaterSimulator] Update to Position.update_position_status

--- octobot_trading/personal_data/positions/channel/positions_updater_simulator.py
+++ octobot_trading/personal_data/positions/channel/positions_updater_simulator.py
@@ -16,10 +16,8 @@
 import copy
 
 import octobot_commons.logging as logging
-
 import octobot_trading.constants as constants
 import octobot_trading.exchange_channel as exchanges_channel
-import octobot_trading.personal_data.positions as positions
 import octobot_trading.personal_data.positions.channel.positions_updater as positions_updater
 
 
@@ -45,38 +43,9 @@ class PositionsUpdaterSimulator(positions_updater.PositionsUpdater):
         MarkPrice channel consumer callback
         """
         try:
-            await self._update_positions_status(cryptocurrency=cryptocurrency, symbol=symbol, mark_price=mark_price)
+            symbol_position = self.channel.exchange_manager.exchange_personal_data.positions_manager \
+                .get_symbol_position(symbol=symbol)
+            symbol_position.mark_price = mark_price
+            await symbol_position.update_position_status(mark_price)
         except Exception as e:
             self.logger.exception(e, True, f"Fail to handle mark price : {e}")
-
-    async def _update_positions_status(self, cryptocurrency: str, symbol: str, mark_price):
-        """
-        Ask positions to check their status
-        Ask liquidation and P&L update process if required
-        """
-        for position in copy.copy(
-                self.channel.exchange_manager.exchange_personal_data.positions_manager.get_open_positions(
-                    symbol=symbol)):
-            await self._update_position_status(position, mark_price)
-
-            if position.is_liquidated():
-                await exchanges_channel.get_chan(constants.POSITIONS_CHANNEL, self.channel.exchange_manager.id) \
-                    .get_internal_producer().send(cryptocurrency=cryptocurrency,
-                                                  symbol=position.symbol,
-                                                  order=position.to_dict(),
-                                                  is_liquidated=position.is_liquidated(),
-                                                  is_updated=False)
-
-    async def _update_position_status(self, position: positions.Position, mark_price):
-        """
-        Call position status update
-        """
-        try:
-            position.mark_price = mark_price
-            await position.update_position_status()
-
-            if position.is_liquidated():
-                self.logger.debug(f"{position.symbol} (ID : {position.position_id})"
-                                  f" liquidated on {self.channel.exchange.name} at {position.mark_price} ")
-        except Exception as e:
-            self.logger.exception(e, True, f"Fail to update position status : {e} (concerned position : {position}).")
