dacd004b90dd1766f42dd6768298ea018c9d1a37
herklos <herklos.dev@protonmail.com>
2021-10-24 19:10:51 +0200

    [FutureAsset] Add Asset implementation

--- tests/personal_data/portfolios/assets/test_future_asset.py
+++ tests/personal_data/portfolios/assets/test_future_asset.py
@@ -13,3 +13,64 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+import decimal
+
+import octobot_trading.constants as constants
+import octobot_trading.personal_data.portfolios.assets.future_asset as future_asset
+
+ASSET_CURRENCY_NAME = "BTC"
+
+
+def test___eq__():
+    asset = future_asset.FutureAsset(ASSET_CURRENCY_NAME, constants.ZERO, constants.ZERO)
+    # TODO
+
+
+def test_update():
+    asset = future_asset.FutureAsset(ASSET_CURRENCY_NAME,
+                                     available=constants.ZERO, total=constants.ZERO,
+                                     initial_margin=constants.ZERO, margin_balance=constants.ZERO,
+                                     maintenance_margin=constants.ZERO, position_initial_margin=constants.ZERO)
+    assert not asset.update(available=constants.ZERO, total=constants.ZERO, margin_balance=constants.ZERO)
+    assert asset.update(available=constants.ZERO, total=decimal.Decimal(5), margin_balance=decimal.Decimal(10))
+    assert asset.total == decimal.Decimal(5)
+    assert asset.margin_balance == decimal.Decimal(10)
+    assert asset.update(available=constants.ZERO, total=constants.ZERO, margin_balance=decimal.Decimal(10),
+                        maintenance_margin=decimal.Decimal(3))
+    assert asset.margin_balance == decimal.Decimal(20)
+    assert asset.total == decimal.Decimal(5)
+    assert asset.maintenance_margin == decimal.Decimal(3)
+    assert asset.available == constants.ZERO
+
+
+def test_set():
+    asset = future_asset.FutureAsset(ASSET_CURRENCY_NAME,
+                                     available=constants.ZERO, total=constants.ZERO,
+                                     initial_margin=constants.ZERO, margin_balance=constants.ZERO,
+                                     maintenance_margin=constants.ZERO, position_initial_margin=constants.ZERO)
+    assert not asset.set(available=constants.ZERO, total=constants.ZERO, margin_balance=constants.ZERO)
+    assert asset.set(available=decimal.Decimal(5), total=decimal.Decimal(5), margin_balance=decimal.Decimal(2))
+    assert not asset.set(available=decimal.Decimal(5), total=decimal.Decimal(5), margin_balance=decimal.Decimal(2))
+    assert asset.available == decimal.Decimal(5)
+    assert asset.total == decimal.Decimal(5)
+    assert asset.margin_balance == decimal.Decimal(2)
+
+
+def test_restore_available():
+    asset = future_asset.FutureAsset(ASSET_CURRENCY_NAME, available=constants.ONE, total=constants.ONE_HUNDRED)
+    assert not asset.available == asset.total
+    asset.restore_available()
+    assert asset.available == asset.total == constants.ONE_HUNDRED
+
+
+def test_reset():
+    asset = future_asset.FutureAsset(ASSET_CURRENCY_NAME,
+                                     available=constants.ONE_HUNDRED, total=constants.ONE_HUNDRED,
+                                     initial_margin=constants.ONE_HUNDRED, margin_balance=constants.ONE_HUNDRED,
+                                     maintenance_margin=constants.ONE_HUNDRED,
+                                     position_initial_margin=constants.ONE_HUNDRED)
+    assert asset.available == asset.total == asset.initial_margin == asset.margin_balance == \
+           asset.maintenance_margin == asset.position_initial_margin == constants.ONE_HUNDRED
+    asset.reset()
+    assert asset.available == asset.total == asset.initial_margin == asset.margin_balance == \
+           asset.maintenance_margin == asset.position_initial_margin == constants.ZERO
