69d8af9bc8f8d01ae6111e3b63c7c7036c34395e
herklos <herklos.dev@protonmail.com>
2021-11-03 22:12:04 +0100

    [PositionsManager] Add get_order_position

--- tests/personal_data/positions/types/test_inverse_position.py
+++ tests/personal_data/positions/types/test_inverse_position.py
@@ -32,13 +32,13 @@ pytestmark = pytest.mark.asyncio
 async def test_update_value(future_trader_simulator):
     config, exchange_manager_inst, trader_inst = future_trader_simulator
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
-    await position_inst.update(update_quantity=constants.ZERO)
+    position_inst.update(update_quantity=constants.ZERO)
     position_inst.update_value()
     assert position_inst.value == constants.ZERO
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED)
     position_inst.update_value()
     assert position_inst.value == constants.ZERO
-    await position_inst.update(mark_price=constants.ONE_HUNDRED)
+    position_inst.update(mark_price=constants.ONE_HUNDRED)
     position_inst.update_value()
     assert position_inst.value == constants.ONE
 
@@ -49,10 +49,10 @@ async def test_update_pnl(future_trader_simulator):
     # long test
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
     position_inst.entry_price = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     position_inst.update_pnl()
     assert position_inst.unrealised_pnl == constants.ZERO
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED,
+    position_inst.update(update_quantity=constants.ONE_HUNDRED,
                                mark_price=decimal.Decimal(2) * constants.ONE_HUNDRED)
     position_inst.update_pnl()
     assert position_inst.unrealised_pnl == constants.ONE
@@ -60,10 +60,10 @@ async def test_update_pnl(future_trader_simulator):
     # short test
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
     position_inst.entry_price = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=-constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=-constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     position_inst.update_pnl()
     assert position_inst.unrealised_pnl == constants.ZERO
-    await position_inst.update(update_quantity=-constants.ONE_HUNDRED,
+    position_inst.update(update_quantity=-constants.ONE_HUNDRED,
                                mark_price=constants.ONE_HUNDRED / decimal.Decimal(10))
     position_inst.update_pnl()
     assert position_inst.unrealised_pnl == decimal.Decimal("-18.00")
@@ -73,14 +73,14 @@ async def test_update_initial_margin(future_trader_simulator):
     config, exchange_manager_inst, trader_inst = future_trader_simulator
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
 
-    await position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
+    position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
     position_inst.update_initial_margin()
     assert position_inst.initial_margin == constants.ZERO
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     position_inst.update_initial_margin()
     assert position_inst.initial_margin == constants.ONE
     position_inst.leverage = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     position_inst.update_initial_margin()
     assert position_inst.initial_margin == decimal.Decimal("0.02")
 
@@ -90,13 +90,13 @@ async def test_calculate_maintenance_margin(future_trader_simulator):
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
 
     position_inst.symbol = DEFAULT_FUTURE_SYMBOL
-    await position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
+    position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
     assert position_inst.calculate_maintenance_margin() == constants.ZERO
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     assert position_inst.calculate_maintenance_margin() == constants.ZERO
     exchange_manager_inst.exchange_symbols_data.get_exchange_symbol_data(
         DEFAULT_FUTURE_SYMBOL).funding_manager.funding_rate = decimal.Decimal(DEFAULT_FUTURE_FUNDING_RATE)
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     assert position_inst.calculate_maintenance_margin() == decimal.Decimal("0.02000000000000000041633363423")
 
 
@@ -109,11 +109,11 @@ async def test_update_isolated_liquidation_price(future_trader_simulator):
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
     position_inst.symbol = DEFAULT_FUTURE_SYMBOL
     position_inst.entry_price = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     position_inst.update_isolated_liquidation_price()
     assert position_inst.liquidation_price == decimal.Decimal("50.25125628140703518113600205")
     position_inst.leverage = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED,
+    position_inst.update(update_quantity=constants.ONE_HUNDRED,
                                mark_price=decimal.Decimal(2) * constants.ONE_HUNDRED)
     position_inst.update_isolated_liquidation_price()
     assert position_inst.liquidation_price == decimal.Decimal("100.0000000000000000208166817")
@@ -122,11 +122,11 @@ async def test_update_isolated_liquidation_price(future_trader_simulator):
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
     position_inst.symbol = DEFAULT_FUTURE_SYMBOL
     position_inst.entry_price = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=-constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=-constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     position_inst.update_isolated_liquidation_price()
     assert position_inst.liquidation_price == decimal.Decimal("9999.999999999999791833182880")
     position_inst.leverage = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=-constants.ONE_HUNDRED,
+    position_inst.update(update_quantity=-constants.ONE_HUNDRED,
                                mark_price=constants.ONE_HUNDRED / decimal.Decimal(10))
     position_inst.update_isolated_liquidation_price()
     assert position_inst.liquidation_price == decimal.Decimal("99.99999999999999997918331830")
@@ -138,13 +138,13 @@ async def test_get_bankruptcy_price(future_trader_simulator):
     # long test
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
     position_inst.entry_price = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     assert position_inst.get_bankruptcy_price() == decimal.Decimal(50)
     assert position_inst.get_bankruptcy_price(with_mark_price=True) == decimal.Decimal(50)
     position_inst.leverage = constants.ONE_HUNDRED
     assert position_inst.get_bankruptcy_price() == decimal.Decimal("99.00990099009900990099009901")
     assert position_inst.get_bankruptcy_price(with_mark_price=True) == decimal.Decimal("0.9900990099009900990099009901")
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED,
+    position_inst.update(update_quantity=constants.ONE_HUNDRED,
                                mark_price=decimal.Decimal(2) * constants.ONE_HUNDRED)
     assert position_inst.get_bankruptcy_price() == decimal.Decimal("99.00990099009900990099009901")
     assert position_inst.get_bankruptcy_price(with_mark_price=True) == decimal.Decimal("1.980198019801980198019801980")
@@ -152,13 +152,13 @@ async def test_get_bankruptcy_price(future_trader_simulator):
     # short test
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
     position_inst.entry_price = constants.ONE_HUNDRED
-    await position_inst.update(update_quantity=-constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=-constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     assert position_inst.get_bankruptcy_price() == constants.ZERO
     assert position_inst.get_bankruptcy_price(with_mark_price=True) == constants.ZERO
     position_inst.leverage = constants.ONE_HUNDRED
     assert position_inst.get_bankruptcy_price() == decimal.Decimal("101.0101010101010101010101010")
     assert position_inst.get_bankruptcy_price(with_mark_price=True) == decimal.Decimal("1.010101010101010101010101010")
-    await position_inst.update(update_quantity=-constants.ONE_HUNDRED,
+    position_inst.update(update_quantity=-constants.ONE_HUNDRED,
                                mark_price=decimal.Decimal(2) * constants.ONE_HUNDRED)
     assert position_inst.get_bankruptcy_price() == constants.ZERO
     assert position_inst.get_bankruptcy_price(with_mark_price=True) == constants.ZERO
@@ -168,9 +168,9 @@ async def test_get_order_cost(future_trader_simulator):
     config, exchange_manager_inst, trader_inst = future_trader_simulator
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
 
-    await position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
+    position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
     assert position_inst.get_order_cost() == constants.ZERO
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     assert position_inst.get_order_cost() == decimal.Decimal("1.003")
 
 
@@ -178,9 +178,9 @@ async def test_get_fee_to_open(future_trader_simulator):
     config, exchange_manager_inst, trader_inst = future_trader_simulator
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
 
-    await position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
+    position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
     assert position_inst.get_fee_to_open() == constants.ZERO
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     assert position_inst.get_fee_to_open() == decimal.Decimal("0.001")
 
 
@@ -188,9 +188,9 @@ async def test_update_fee_to_close(future_trader_simulator):
     config, exchange_manager_inst, trader_inst = future_trader_simulator
     position_inst = personal_data.InversePosition(trader_inst, DEFAULT_FUTURE_SYMBOL_CONTRACT)
 
-    await position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
+    position_inst.update(update_quantity=constants.ZERO, mark_price=constants.ZERO)
     position_inst.update_fee_to_close()
     assert position_inst.fee_to_close == constants.ZERO
-    await position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
+    position_inst.update(update_quantity=constants.ONE_HUNDRED, mark_price=constants.ONE_HUNDRED)
     position_inst.update_fee_to_close()
     assert position_inst.fee_to_close == decimal.Decimal("0.002")
