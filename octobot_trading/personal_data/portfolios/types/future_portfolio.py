c4c2b9c3cd19295d9444dfc566b2571ef64cd15f
Herklos <herklos.dev@protonmail.com>
2021-11-13 17:48:47 +0100

    [FuturePortfolio] Add _update_future_portfolio_data

--- octobot_trading/personal_data/portfolios/types/future_portfolio.py
+++ octobot_trading/personal_data/portfolios/types/future_portfolio.py
@@ -45,22 +45,22 @@ class FuturePortfolio(portfolio_class.Portfolio):
             if pair_future_contract.is_inverse_contract():
                 total_update_quantity = real_order_quantity / order.filled_price
                 fees_update_quantity = -order.get_total_fees(order.currency)
-                self._update_portfolio_data(
+                self._update_future_portfolio_data(
                     order.currency,
-                    available_value=fees_update_quantity + (
-                        constants.ZERO if have_increased_position_size else total_update_quantity),
-                    total_value=fees_update_quantity)
+                    wallet_value=fees_update_quantity,
+                    order_margin_value=-total_update_quantity,
+                    position_margin_value=total_update_quantity if have_increased_position_size else constants.ZERO)
 
             # When non-inverse contract, decrease directly market quantity
             else:
                 # decrease market quantity from market available balance
                 total_update_quantity = real_order_quantity * order.filled_price
                 fees_update_quantity = -order.get_total_fees(order.market)
-                self._update_portfolio_data(
+                self._update_future_portfolio_data(
                     order.market,
-                    available_value=fees_update_quantity + (
-                        constants.ZERO if have_increased_position_size else total_update_quantity),
-                    total_value=fees_update_quantity)
+                    wallet_value=fees_update_quantity,
+                    order_margin_value=-total_update_quantity,
+                    position_margin_value=total_update_quantity if have_increased_position_size else constants.ZERO)
         except (decimal.DivisionByZero, decimal.InvalidOperation) as e:
             self.logger.error(f"Failed to update from filled order : {order} ({e})")
 
@@ -72,20 +72,18 @@ class FuturePortfolio(portfolio_class.Portfolio):
         """
         pair_future_contract = order.exchange_manager.exchange.get_pair_future_contract(order.symbol)
         try:
-            real_order_quantity = - (order.origin_quantity / pair_future_contract.current_leverage
-                                     * (constants.ONE if is_new_order else -constants.ONE))
+            real_order_quantity = (order.origin_quantity / pair_future_contract.current_leverage
+                                   * (constants.ONE if is_new_order else -constants.ONE))
 
             # When inverse contract, decrease a currency market equivalent quantity from currency balance
             if pair_future_contract.is_inverse_contract():
-                self._update_portfolio_data(order.currency,
-                                            available_value=real_order_quantity / order.origin_price,
-                                            total_value=constants.ZERO)
+                self._update_future_portfolio_data(order.currency,
+                                                   order_margin_value=real_order_quantity / order.origin_price)
             # When non-inverse contract, decrease directly market quantity
             else:
                 # decrease market quantity from market available balance
-                self._update_portfolio_data(order.market,
-                                            available_value=real_order_quantity * order.origin_price,
-                                            total_value=constants.ZERO)
+                self._update_future_portfolio_data(order.market,
+                                                   order_margin_value=real_order_quantity * order.origin_price)
         except (decimal.DivisionByZero, decimal.InvalidOperation) as e:
             self.logger.error(f"Failed to update available from order : {order} ({e})")
 
@@ -99,14 +97,14 @@ class FuturePortfolio(portfolio_class.Portfolio):
                 position.get_quantity_to_close() / position.symbol_contract.current_leverage).copy_abs()
             if position.symbol_contract.is_inverse_contract():
                 update_quantity = liquidated_quantity / position.mark_price
-                self._update_portfolio_data(position.currency,
-                                            available_value=update_quantity,
-                                            total_value=update_quantity)
+                self._update_future_portfolio_data(position.currency,
+                                                   position_margin_value=update_quantity,
+                                                   wallet_value=update_quantity)
             else:
                 update_quantity = liquidated_quantity * position.mark_price
-                self._update_portfolio_data(position.market,
-                                            available_value=update_quantity,
-                                            total_value=update_quantity)
+                self._update_future_portfolio_data(position.market,
+                                                   position_margin_value=update_quantity,
+                                                   wallet_value=update_quantity)
         except (decimal.DivisionByZero, decimal.InvalidOperation) as e:
             self.logger.error(f"Failed to update from liquidated position : {position} ({e})")
 
@@ -120,13 +118,45 @@ class FuturePortfolio(portfolio_class.Portfolio):
             funding_fee = position.value * funding_rate
             # When inverse contract, decrease a currency market equivalent quantity from currency balance
             if position.symbol_contract.is_inverse_contract():
-                self._update_portfolio_data(position.currency,
-                                            available_value=-funding_fee,
-                                            total_value=-funding_fee)
+                self._update_future_portfolio_data(position.currency, wallet_value=-funding_fee)
             # When non-inverse contract, decrease directly market quantity
             else:
-                self._update_portfolio_data(position.market,
-                                            available_value=-funding_fee,
-                                            total_value=-funding_fee)
+                self._update_future_portfolio_data(position.market, wallet_value=-funding_fee)
         except (decimal.DivisionByZero, decimal.InvalidOperation) as e:
             self.logger.error(f"Failed to update from funding : {position} ({e})")
+
+    def _update_future_portfolio_data(self, currency,
+                                      wallet_value=constants.ZERO,
+                                      position_margin_value=constants.ZERO,
+                                      order_margin_value=constants.ZERO,
+                                      unrealized_pnl_value=constants.ZERO,
+                                      initial_margin_value=constants.ZERO,
+                                      replace_value=False):
+        """
+        Set new currency quantity in the portfolio
+        :param currency: the currency to update
+        :param wallet_value: the wallet balance value
+        :param initial_margin_value: the initial margin value
+        :param position_margin_value: the position margin value
+        :param order_margin_value: the order margin value
+        :param unrealized_pnl_value: the unrealized pnl value
+        :param replace_value: when True replace the current value instead of updating it
+        :return: True if updated
+        """
+        try:
+            if replace_value:
+                return self.portfolio[currency].set(total=wallet_value,
+                                                    position_margin=position_margin_value,
+                                                    initial_margin=initial_margin_value,
+                                                    available=order_margin_value,
+                                                    unrealized_pnl=unrealized_pnl_value)
+            return self.portfolio[currency].update(total=wallet_value,
+                                                   position_margin=position_margin_value,
+                                                   initial_margin=initial_margin_value,
+                                                   available=order_margin_value,
+                                                   unrealized_pnl=unrealized_pnl_value)
+        except KeyError:
+            self.portfolio[currency] = self.create_currency_asset(currency=currency,
+                                                                  available=order_margin_value,
+                                                                  total=wallet_value)
+            return True
