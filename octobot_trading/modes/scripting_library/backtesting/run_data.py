f9ff06b5d6633af4d90629b39074c483d6356d81
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-12-01 21:39:37 +0100

    [Scripting] handle multi symbol run data

--- octobot_trading/modes/scripting_library/backtesting/run_data.py
+++ octobot_trading/modes/scripting_library/backtesting/run_data.py
@@ -167,57 +167,59 @@ async def plot_historical_pnl_value(meta_database, plotted_element, exchange=Non
         return
     x_data = [0 if x_as_trade_count else next(iter(price_data.values()))[0][commons_enums.PriceIndexes.IND_PRICE_TIME.value]]
     pnl_data = [0]
-    for pair in price_data.keys():
-        symbol, ref_market = symbol_util.split_symbol(pair)
-        buy_order_volume_by_price = {}
-        long_position_size = 0
-        for trade in sorted(trades_data[pair], key=lambda x: x[trading_enums.PlotAttributes.X.value]):
-            if trade[trading_enums.DBTables.SYMBOL.value] == pair:
-                trade_volume = trade[trading_enums.PlotAttributes.VOLUME.value]
-                if trade[trading_enums.PlotAttributes.SIDE.value] == trading_enums.TradeOrderSide.BUY.value:
-                    if trade[trading_enums.PlotAttributes.Y.value] in buy_order_volume_by_price:
-                        buy_order_volume_by_price[trade[trading_enums.PlotAttributes.Y.value]] += trade_volume
-                    else:
-                        buy_order_volume_by_price[trade[trading_enums.PlotAttributes.Y.value]] = trade_volume
-                    # increase position size
-                    long_position_size += trade_volume * trade[trading_enums.PlotAttributes.Y.value]
-                elif trade[trading_enums.PlotAttributes.SIDE.value] == trading_enums.TradeOrderSide.SELL.value:
-                    remaining_sell_volume = trade_volume
-                    volume_by_bought_prices = {}
-                    for order_price in sorted(buy_order_volume_by_price.keys()):
-                        if buy_order_volume_by_price[order_price] > remaining_sell_volume:
-                            buy_order_volume_by_price[order_price] -= remaining_sell_volume
-                            volume_by_bought_prices[order_price] = remaining_sell_volume
-                            remaining_sell_volume = 0
-                        elif buy_order_volume_by_price[order_price] == remaining_sell_volume:
-                            buy_order_volume_by_price.pop(order_price)
-                            volume_by_bought_prices[order_price] = remaining_sell_volume
-                            remaining_sell_volume = 0
-                        else:
-                            # buy_order_volume_by_price[order_price] < remaining_sell_volume
-                            buy_volume = buy_order_volume_by_price.pop(order_price)
-                            volume_by_bought_prices[order_price] = buy_volume
-                            remaining_sell_volume -= buy_volume
-                        if remaining_sell_volume <= 0:
-                            break
-                    if volume_by_bought_prices:
-                        # use total_bought_volume only to avoid taking pre-existing open positions into account
-                        # (ex if started with already 10 btc)
-                        total_bought_volume = sum(volume for volume in volume_by_bought_prices.values())
-                        average_buy_price = sum(price * (volume/total_bought_volume)
-                                                for price, volume in volume_by_bought_prices.items())
-                        fees = trade[trading_enums.DBTables.FEES_AMOUNT.value]
-                        fees_multiplier = 1 if trade[trading_enums.DBTables.FEES_CURRENCY.value] == ref_market \
-                            else trade[trading_enums.PlotAttributes.Y.value]
-                        pnl = (trade[trading_enums.PlotAttributes.Y.value] - average_buy_price) * total_bought_volume - \
-                              fees * fees_multiplier
-                        pnl_data.append(pnl)
-                        if x_as_trade_count:
-                            x_data.append(len(pnl_data) - 1)
-                        else:
-                            x_data.append(trade[trading_enums.PlotAttributes.X.value])
+    buy_order_volume_by_price_by_currency = {
+        symbol_util.split_symbol(symbol)[0]: {}
+        for symbol in trades_data.keys()
+    }
+    all_trades = []
+    for trades in trades_data.values():
+        all_trades += trades
+    for trade in sorted(all_trades, key=lambda x: x[trading_enums.PlotAttributes.X.value]):
+        currency, ref_market = symbol_util.split_symbol(trade[trading_enums.DBTables.SYMBOL.value])
+        trade_volume = trade[trading_enums.PlotAttributes.VOLUME.value]
+        buy_order_volume_by_price = buy_order_volume_by_price_by_currency[currency]
+        if trade[trading_enums.PlotAttributes.SIDE.value] == trading_enums.TradeOrderSide.BUY.value:
+            if trade[trading_enums.PlotAttributes.Y.value] in buy_order_volume_by_price:
+                buy_order_volume_by_price[trade[trading_enums.PlotAttributes.Y.value]] += trade_volume
             else:
-                raise RuntimeError(f"Unknown trade side: {trade}")
+                buy_order_volume_by_price[trade[trading_enums.PlotAttributes.Y.value]] = trade_volume
+        elif trade[trading_enums.PlotAttributes.SIDE.value] == trading_enums.TradeOrderSide.SELL.value:
+            remaining_sell_volume = trade_volume
+            volume_by_bought_prices = {}
+            for order_price in sorted(buy_order_volume_by_price.keys()):
+                if buy_order_volume_by_price[order_price] > remaining_sell_volume:
+                    buy_order_volume_by_price[order_price] -= remaining_sell_volume
+                    volume_by_bought_prices[order_price] = remaining_sell_volume
+                    remaining_sell_volume = 0
+                elif buy_order_volume_by_price[order_price] == remaining_sell_volume:
+                    buy_order_volume_by_price.pop(order_price)
+                    volume_by_bought_prices[order_price] = remaining_sell_volume
+                    remaining_sell_volume = 0
+                else:
+                    # buy_order_volume_by_price[order_price] < remaining_sell_volume
+                    buy_volume = buy_order_volume_by_price.pop(order_price)
+                    volume_by_bought_prices[order_price] = buy_volume
+                    remaining_sell_volume -= buy_volume
+                if remaining_sell_volume <= 0:
+                    break
+            if volume_by_bought_prices:
+                # use total_bought_volume only to avoid taking pre-existing open positions into account
+                # (ex if started with already 10 btc)
+                total_bought_volume = sum(volume for volume in volume_by_bought_prices.values())
+                average_buy_price = sum(price * (volume/total_bought_volume)
+                                        for price, volume in volume_by_bought_prices.items())
+                fees = trade[trading_enums.DBTables.FEES_AMOUNT.value]
+                fees_multiplier = 1 if trade[trading_enums.DBTables.FEES_CURRENCY.value] == ref_market \
+                    else trade[trading_enums.PlotAttributes.Y.value]
+                pnl = (trade[trading_enums.PlotAttributes.Y.value] - average_buy_price) * total_bought_volume - \
+                      fees * fees_multiplier
+                pnl_data.append(pnl)
+                if x_as_trade_count:
+                    x_data.append(len(pnl_data) - 1)
+                else:
+                    x_data.append(trade[trading_enums.PlotAttributes.X.value])
+        else:
+            get_logger().error(f"Unknown trade side: {trade}")
 
     plotted_element.plot(
         kind="scatter",
