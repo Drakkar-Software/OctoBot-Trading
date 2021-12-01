6e65614b0a5a7e13e4409750ceb1a53401fe0b11
max <max@tfq.at>
2021-12-01 02:29:26 +0000

    [Scripting] add delay to channel

--- octobot_trading/modes/scripting_library/TA/trigger/channel/channel.py
+++ octobot_trading/modes/scripting_library/TA/trigger/channel/channel.py
@@ -15,36 +15,69 @@
 #  License along with this library.
 
 
-def entering_channel_up(price, val1, val2):
-    if val1 < val2:
-        return price[-1] > val1[-1] and price[-2] < val1[-2]
-    if val1 > val2:
-        return price[-1] > val2[-1] and price[-2] < val2[-2]
+def entering_channel_up(price, val1, val2, delay=0):
+    condition = False
+    delay = delay + 1
+    for i in range(0, delay):
+        if val1 < val2:
+            condition = price[-i] > val1[-i] and price[-i + 1] < val1[-i + 1]
+            if not condition:
+                return
+        if val1 > val2:
+            condition = price[-i] > val2[-i] and price[-i + 1] < val2[-i + 1]
+            if not condition:
+                return
+    return condition
 
 
-def entering_channel_down(price, val1, val2):
-    if val1 > val2:
-        return price[-1] < val1[-1] and price[-2] > val1[-2]
-    if val1 < val2:
-        return price[-1] < val2[-1] and price[-2] > val2[-2]
+def entering_channel_down(price, val1, val2, delay=0):
+    condition = False
+    delay = delay + 1
+    for i in range(0, delay):
+        if val1 > val2:
+            condition = price[-i] < val1[-i] and price[-i + 1] > val1[-i + 1]
+            if not condition:
+                return False
+        if val1 < val2:
+            condition = price[-i] < val2[-i] and price[-i + 1] > val2[-i + 1]
+            if not condition:
+                return False
+    return condition
 
 
-def entering_channel(price, val1, val2):
-    if entering_channel_up(price, val1, val2):
+def entering_channel(price, val1, val2, delay=0):
+    delay = delay + 1
+    if entering_channel_up(price, val1, val2, delay):
         return True
-    if entering_channel_down(price, val1, val2):
+    if entering_channel_down(price, val1, val2, delay):
         return True
 
 
-def inside_channel(price, val1, val2):
-    if val1 < val2:
-        return val1[-1] < price[-1] < val2[-1]
-    if val1 > val2:
-        return val1[-1] > price[-1] > val2[-1]
+def inside_channel(price, val1, val2, delay=0):
+    condition = False
+    delay = delay + 1
+    for i in range(0, delay):
+        if val1 < val2:
+            condition = val1[-i] < price[-i] < val2[-i]
+            if not condition:
+                return False
+        if val1 > val2:
+            condition = val1[-i] > price[-i] > val2[-i]
+        if not condition:
+            return False
+    return condition
 
 
-def outside_channel(price, val1, val2):
-    if val1 < val2:
-        return val1[-1] > price[-1] > val2[-1]
-    if val1 > val2:
-        return val1[-1] < price[-1] < val2[-1]
+def outside_channel(price, val1, val2, delay=0):
+    condition = False
+    delay = delay + 1
+    for i in range(0, delay):
+        if val1 < val2:
+            condition = val1[-i] > price[-i] > val2[-i]
+            if not condition:
+                return False
+        if val1 > val2:
+            condition = val1[-i] < price[-i] < val2[-i]
+        if not condition:
+            return False
+    return condition
