09b6d7a0a8df991c109d087c6cc517bae39f8974
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-12-01 23:46:18 +0100

    [ScriptedTrading] handle optimizer metadata

--- octobot_trading/modes/scripting_library/backtesting/metadata.py
+++ octobot_trading/modes/scripting_library/backtesting/metadata.py
@@ -22,11 +22,19 @@ def set_script_name(ctx, name):
     ctx.tentacle.script_name = name
 
 
-async def read_metadata(ctx=None, trading_mode=None, optimizer_id=None):
+async def read_metadata(ctx=None, trading_mode=None, include_optimizer_runs=False):
     trading_mode = trading_mode or ctx.trading_mode_class
-    database_manager = databases.DatabaseManager(trading_mode, backtesting_id="1", optimizer_id=optimizer_id)
-    try:
-        async with data.MetadataReader.database(database_manager.get_backtesting_metadata_identifier()) as reader:
-            return await reader.read()
-    except commons_errors.DatabaseNotFoundError:
-        return []
+    metadata = []
+    optimizer_data_managers = []
+    backtesting_database_manager = databases.DatabaseManager(trading_mode, backtesting_id="1")
+    if include_optimizer_runs:
+        optimizer_ids = await backtesting_database_manager.get_optimizer_run_ids()
+        optimizer_data_managers = [databases.DatabaseManager(trading_mode, optimizer_id=optimizer_id)
+                                   for optimizer_id in optimizer_ids]
+    for database_manager in [backtesting_database_manager] + optimizer_data_managers:
+        try:
+            async with data.MetadataReader.database(database_manager.get_backtesting_metadata_identifier()) as reader:
+                metadata += await reader.read()
+        except commons_errors.DatabaseNotFoundError:
+            pass
+    return metadata
