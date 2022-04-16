ac3d00f28d4a0896963ca781afd95f84e52bfc45
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-04-16 21:59:46 +0200

    [Storage] manage storage from api and exchange builder

--- octobot_trading/util/databases_util.py
+++ octobot_trading/util/databases_util.py
@@ -18,13 +18,13 @@ import octobot_commons.optimization_campaign as optimization_campaign
 import octobot_commons.constants as commons_constants
 
 
-def get_run_databases_identifier(exchange_manager, trading_mode_class=None):
+def get_run_databases_identifier(config, tentacles_setup_config, trading_mode_class=None):
     # avoid importing octobot_trading.modes directly to prevent circular imports
     import octobot_trading.modes as modes
 
     return databases.RunDatabasesIdentifier(
-        trading_mode_class or modes.get_activated_trading_mode(exchange_manager.tentacles_setup_config),
-        optimization_campaign.OptimizationCampaign.get_campaign_name(exchange_manager.tentacles_setup_config),
-        backtesting_id=exchange_manager.config.get(commons_constants.CONFIG_BACKTESTING_ID),
-        optimizer_id=exchange_manager.config.get(commons_constants.CONFIG_OPTIMIZER_ID)
+        trading_mode_class or modes.get_activated_trading_mode(tentacles_setup_config),
+        optimization_campaign.OptimizationCampaign.get_campaign_name(tentacles_setup_config),
+        backtesting_id=config.get(commons_constants.CONFIG_BACKTESTING_ID),
+        optimizer_id=config.get(commons_constants.CONFIG_OPTIMIZER_ID)
     )
