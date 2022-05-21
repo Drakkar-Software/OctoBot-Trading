310dcf60ec51edeedc2da97d9052ee6d59dc317f
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-05-21 20:38:42 +0200

    [Tests] add exchange builder test

--- tests/exchanges/test_exchange_builder.py
+++ tests/exchanges/test_exchange_builder.py
@@ -14,6 +14,7 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 import pytest
+import octobot_trading.api as api
 import octobot_trading.exchanges as exchanges
 
 from tests.exchanges import exchange_manager
@@ -37,3 +38,20 @@ async def test_build_trading_modes_if_required(exchange_manager):
     builder.is_simulated()
     with pytest.raises(AttributeError):
         await builder._build_trading_modes_if_required(None)
+
+
+async def test_build_collector_exchange(exchange_manager):
+    new_exchange_manager = await api.create_exchange_builder(exchange_manager.config, exchange_manager.exchange_name) \
+        .is_simulated() \
+        .is_rest_only() \
+        .is_exchange_only() \
+        .is_collecting() \
+        .is_ignoring_config() \
+        .disable_trading_mode() \
+        .use_tentacles_setup_config(exchange_manager.tentacles_setup_config) \
+        .build()
+    assert new_exchange_manager is not exchange_manager
+    assert new_exchange_manager.config is exchange_manager.config
+    assert new_exchange_manager.exchange_name == exchange_manager.exchange_name
+    assert new_exchange_manager.tentacles_setup_config is exchange_manager.tentacles_setup_config
+    await new_exchange_manager.stop()
