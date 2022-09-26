6deb0ce0e1b37540f52174330a704f4ba8ce1825
Max <max@tfq.at>
2022-09-26 18:44:38 +0200

    [RunData] simplify run metadata
    [RunData] add get additional metadata

--- octobot_trading/modes/abstract_trading_mode.py
+++ octobot_trading/modes/abstract_trading_mode.py
@@ -343,7 +343,7 @@ class AbstractTradingMode(abstract_tentacle.AbstractTentacle):
     async def save_live_metadata(self):
         await basic_keywords.save_metadata(
             databases.RunDatabasesProvider.instance().get_run_db(self.exchange_manager.bot_id),
-            await self.get_live_metadata()
+            await self.get_trading_metadata(is_backtesting=False)
         )
 
     async def save_candles_data(self):
@@ -610,16 +610,43 @@ class AbstractTradingMode(abstract_tentacle.AbstractTentacle):
                     async with databases.DBWriter.database(
                             run_dbs_identifier.get_backtesting_metadata_identifier(),
                             with_lock=True) as writer:
-                        await basic_keywords.save_metadata(writer, await self.get_backtesting_metadata(
+                        await basic_keywords.save_metadata(writer, await self.get_trading_metadata(
                             user_inputs,
-                            run_dbs_identifier
+                            run_dbs_identifier,
+                            is_backtesting=True
                         ))
                         self.are_metadata_saved = True
                 except Exception:
                     self.__class__.SAVED_RUN_METADATA_DB_BY_BOT_ID[self.exchange_manager.bot_id] = False
                     raise
 
-    async def get_live_metadata(self):
+    async def get_trading_metadata(self, user_inputs=None, run_dbs_identifier=None, is_backtesting=True) -> dict:
+        """
+        Override this method to replace metadata
+        Overide get_additional_live_metadata or get_additional_backtesting_metadata to add additional metadata
+        :return: the metadata dict related to this backtesting and live run
+        """
+        symbols = self.exchange_manager.exchange_config.traded_symbol_pairs
+        portfolio_manager = self.exchange_manager.exchange_personal_data.portfolio_manager.portfolio_profitability
+        profitability = portfolio_manager.profitability
+        profitability_percent = portfolio_manager.profitability_percent
+        if user_inputs is None:
+            user_inputs = {}
+        origin_portfolio = portfolios.portfolio_to_float(
+            self.exchange_manager.exchange_personal_data.portfolio_manager.
+            portfolio_value_holder.origin_portfolio.portfolio)
+        end_portfolio = portfolios.portfolio_to_float(
+            self.exchange_manager.exchange_personal_data.portfolio_manager.portfolio.portfolio)
+        for portfolio in (origin_portfolio, end_portfolio):
+            for values in portfolio.values():
+                values.pop("available", None)
+        if self.exchange_manager.is_future:
+            for position in self.exchange_manager.exchange_personal_data.positions_manager.positions.values():
+                end_portfolio[position.get_currency()]["position"] = float(position.quantity)
+        time_frames = [
+            tf.value
+            for tf in self.exchange_manager.exchange_config.get_relevant_time_frames()
+        ]
         start_time = backtesting_api.get_backtesting_starting_time(self.exchange_manager.exchange.backtesting) \
             if self.exchange_manager.is_backtesting \
             else self.exchange_manager.exchange.get_exchange_current_time()
@@ -646,43 +673,6 @@ class AbstractTradingMode(abstract_tentacle.AbstractTentacle):
                     if symbol in self.exchange_manager.exchange_config.traded_symbol_pairs
                 }
             }
-        return {
-            **{
-                common_enums.DBRows.REFERENCE_MARKET.value: util.get_reference_market(self.config),
-                common_enums.DBRows.START_TIME.value: start_time,
-                common_enums.DBRows.END_TIME.value: end_time,
-                common_enums.DBRows.TRADING_TYPE.value: exchange_type,
-                common_enums.DBRows.EXCHANGES.value: exchange_names,
-                common_enums.DBRows.FUTURE_CONTRACTS.value: future_contracts_by_exchange,
-                common_enums.DBRows.SYMBOLS.value: self.exchange_manager.exchange_config.traded_symbol_pairs,
-            },
-            **(await self.get_additional_backtesting_metadata())
-        }
-
-    async def get_backtesting_metadata(self, user_inputs, run_dbs_identifier) -> dict:
-        """
-        Override this method to get add addition metadata
-        :return: the metadata dict related to this backtesting run
-        """
-        symbols = self.exchange_manager.exchange_config.traded_symbol_pairs
-        portfolio_manager = self.exchange_manager.exchange_personal_data.portfolio_manager.portfolio_profitability
-        profitability = portfolio_manager.profitability
-        profitability_percent = portfolio_manager.profitability_percent
-        origin_portfolio = portfolios.portfolio_to_float(
-            self.exchange_manager.exchange_personal_data.portfolio_manager.
-            portfolio_value_holder.origin_portfolio.portfolio)
-        end_portfolio = portfolios.portfolio_to_float(
-            self.exchange_manager.exchange_personal_data.portfolio_manager.portfolio.portfolio)
-        for portfolio in (origin_portfolio, end_portfolio):
-            for values in portfolio.values():
-                values.pop("available", None)
-        if self.exchange_manager.is_future:
-            for position in self.exchange_manager.exchange_personal_data.positions_manager.positions.values():
-                end_portfolio[position.get_currency()]["position"] = float(position.quantity)
-        time_frames = [
-            tf.value
-            for tf in self.exchange_manager.exchange_config.get_relevant_time_frames()
-        ]
         formatted_user_inputs = {}
         for user_input in user_inputs:
             if not user_input["is_nested_config"]:
@@ -720,11 +710,18 @@ class AbstractTradingMode(abstract_tentacle.AbstractTentacle):
         except KeyError:
             r_sq_max_balance = None
 
+        backtesting_only_metadata = {
+            common_enums.BacktestingMetadata.ID.value: run_dbs_identifier.backtesting_id,
+            common_enums.BacktestingMetadata.OPTIMIZATION_CAMPAIGN.value: run_dbs_identifier.optimization_campaign_name,
+            common_enums.BacktestingMetadata.DURATION.value: round(backtesting_api.get_backtesting_duration(
+                self.exchange_manager.exchange.backtesting), 3),
+            common_enums.BacktestingMetadata.BACKTESTING_FILES.value:
+                self.exchange_manager.exchange.get_backtesting_data_files(),
+            common_enums.BacktestingMetadata.USER_INPUTS.value: formatted_user_inputs,
+        } if is_backtesting else {}
         return {
+            **backtesting_only_metadata,
             **{
-                common_enums.BacktestingMetadata.OPTIMIZATION_CAMPAIGN.value:
-                    run_dbs_identifier.optimization_campaign_name,
-                common_enums.BacktestingMetadata.ID.value: run_dbs_identifier.backtesting_id,
                 common_enums.BacktestingMetadata.GAINS.value: round(float(profitability), 8),
                 common_enums.BacktestingMetadata.PERCENT_GAINS.value: round(float(profitability_percent), 3),
                 common_enums.BacktestingMetadata.END_PORTFOLIO.value: str(end_portfolio),
@@ -734,13 +731,10 @@ class AbstractTradingMode(abstract_tentacle.AbstractTentacle):
                 common_enums.BacktestingMetadata.COEFFICIENT_OF_DETERMINATION_MAX_BALANCE.value: r_sq_max_balance or 0,
                 common_enums.BacktestingMetadata.COEFFICIENT_OF_DETERMINATION_END_BALANCE.value: r_sq_end_balance or 0,
                 common_enums.BacktestingMetadata.SYMBOLS.value: symbols,
+                common_enums.DBRows.REFERENCE_MARKET.value: util.get_reference_market(self.config),
                 common_enums.BacktestingMetadata.TIME_FRAMES.value: time_frames,
-                common_enums.BacktestingMetadata.START_TIME.value: backtesting_api.get_backtesting_starting_time(
-                    self.exchange_manager.exchange.backtesting),
-                common_enums.BacktestingMetadata.END_TIME.value: backtesting_api.get_backtesting_ending_time(
-                    self.exchange_manager.exchange.backtesting),
-                common_enums.BacktestingMetadata.DURATION.value: round(backtesting_api.get_backtesting_duration(
-                    self.exchange_manager.exchange.backtesting), 3),
+                common_enums.DBRows.START_TIME.value: start_time,
+                common_enums.DBRows.END_TIME.value: end_time,
                 common_enums.BacktestingMetadata.ENTRIES.value: len(entries),
                 common_enums.BacktestingMetadata.WINS.value: wins,
                 common_enums.BacktestingMetadata.LOSES.value: len(entries) - wins,
@@ -748,13 +742,21 @@ class AbstractTradingMode(abstract_tentacle.AbstractTentacle):
                 common_enums.BacktestingMetadata.TIMESTAMP.value: self.start_time,
                 common_enums.BacktestingMetadata.NAME.value: self.get_name(),
                 common_enums.BacktestingMetadata.LEVERAGE.value: leverage,
-                common_enums.BacktestingMetadata.USER_INPUTS.value: formatted_user_inputs,
-                common_enums.BacktestingMetadata.BACKTESTING_FILES.value:
-                    self.exchange_manager.exchange.get_backtesting_data_files(),
-                common_enums.BacktestingMetadata.EXCHANGE.value: self.exchange_manager.exchange_name
+                common_enums.DBRows.TRADING_TYPE.value: exchange_type,
+                common_enums.DBRows.EXCHANGES.value: exchange_names,
+                common_enums.BacktestingMetadata.EXCHANGE.value: self.exchange_manager.exchange_name,
+                common_enums.DBRows.FUTURE_CONTRACTS.value: future_contracts_by_exchange,
             },
-            **(await self.get_additional_backtesting_metadata())
+            **(await self.get_additional_backtesting_metadata() if is_backtesting
+               else await self.get_additional_live_metadata()),
+            **(await self.get_additional_metadata())
         }
+
+    async def get_additional_metadata(self):
+        """
+        Override if necessary
+        """
+        return {}
 
     async def get_additional_live_metadata(self):
         """
