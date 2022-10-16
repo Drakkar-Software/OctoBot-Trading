0dcbdffcd93c1847ff8e8cf272f8965994cc6e9a
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-10-16 18:24:16 +0200

    [CCXT] fix fees computation

--- tests/exchanges/connectors/test_ccxt_exchange.py
+++ tests/exchanges/connectors/test_ccxt_exchange.py
@@ -14,13 +14,18 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+import decimal
+
 import mock
 
 import octobot_trading.exchanges.connectors as exchange_connectors
 import octobot_trading.enums as enums
+import octobot_trading.exchange_data.contracts as contracts
 import pytest
 
-from tests.exchanges import exchange_manager
+from tests.exchanges import exchange_manager, future_simulated_exchange_manager
+from tests.exchanges.traders import future_trader, future_trader_simulator_with_default_linear, DEFAULT_FUTURE_SYMBOL, \
+    DEFAULT_FUTURE_SYMBOL_MARGIN_TYPE, DEFAULT_FUTURE_SYMBOL_LEVERAGE
 
 # All test coroutines will be treated as marked.
 pytestmark = pytest.mark.asyncio
@@ -121,3 +126,45 @@ async def test_get_ccxt_order_type(exchange_manager):
     assert ccxt_exchange.get_ccxt_order_type(enums.TraderOrderType.STOP_LOSS_LIMIT) == enums.TradeOrderType.LIMIT.value
     assert ccxt_exchange.get_ccxt_order_type(enums.TraderOrderType.TRAILING_STOP) == enums.TradeOrderType.MARKET.value
     assert ccxt_exchange.get_ccxt_order_type(enums.TraderOrderType.SELL_MARKET) == enums.TradeOrderType.MARKET.value
+
+
+async def test_get_trade_fee(exchange_manager, future_trader_simulator_with_default_linear):
+    config, fut_exchange_manager_inst, trader_inst, default_contract = future_trader_simulator_with_default_linear
+    fut_exchange_manager_inst.is_future = True
+    fut_ccxt_exchange = exchange_connectors.CCXTExchange(config, fut_exchange_manager_inst)
+    spot_ccxt_exchange = exchange_connectors.CCXTExchange(exchange_manager.config, exchange_manager)
+    # spot trading
+    spot_ccxt_exchange.client.options['defaultType'] = enums.ExchangeTypes.SPOT.value
+    await spot_ccxt_exchange.client.load_markets()
+    assert spot_ccxt_exchange.get_trade_fee("BTC/USDT", enums.TraderOrderType.BUY_LIMIT, decimal.Decimal("0.45"),
+                                            decimal.Decimal(10000), "taker") == \
+           _get_fees("taker", "BTC", 0.001, decimal.Decimal("0.00045"))
+    assert spot_ccxt_exchange.get_trade_fee("BTC/USDT", enums.TraderOrderType.SELL_LIMIT, decimal.Decimal("0.45"),
+                                            decimal.Decimal(10000), "maker") == \
+           _get_fees("maker", "USDT", 0.001, decimal.Decimal("10"))
+    # future trading
+    fut_ccxt_exchange.client.options['defaultType'] = enums.ExchangeTypes.FUTURE.value
+    await fut_ccxt_exchange.client.load_markets()
+    # linear
+    assert fut_ccxt_exchange.get_trade_fee("BTC/USDT", enums.TraderOrderType.BUY_LIMIT, decimal.Decimal("0.45"),
+                                           decimal.Decimal(10000), "taker") == \
+           _get_fees("taker", "USDT", 0.0004, decimal.Decimal("1.8"))
+    # inverse
+    fut_ccxt_exchange.client.markets["BTC/USDT"]["inverse"] = True
+    fut_ccxt_exchange.client.markets["BTC/USDT"]["linear"] = False
+    contract = contracts.FutureContract(pair="BTC/USDT",
+                                        margin_type=enums.MarginType.ISOLATED,
+                                        contract_type=enums.FutureContractType.INVERSE_PERPETUAL)
+    fut_exchange_manager_inst.exchange.pair_contracts["BTC/USDT"] = contract
+    assert fut_ccxt_exchange.get_trade_fee("BTC/USDT", enums.TraderOrderType.BUY_LIMIT, decimal.Decimal("0.45"),
+                                           decimal.Decimal(10000), "taker") == \
+           _get_fees("taker", "BTC", 0.0004, decimal.Decimal("0.00018"))
+
+
+def _get_fees(type, currency, rate, cost):
+    return {
+        enums.FeePropertyColumns.TYPE.value: type,
+        enums.FeePropertyColumns.CURRENCY.value: currency,
+        enums.FeePropertyColumns.RATE.value: rate,
+        enums.FeePropertyColumns.COST.value: decimal.Decimal(str(cost)),
+    }
