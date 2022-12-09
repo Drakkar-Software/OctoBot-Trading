6b0933f6a5570c379d1add9412f408e42c51262e
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-12-09 11:51:05 +0100

    [UserInputs] make trading modes config amount not required

--- octobot_trading/modes/mode_config.py
+++ octobot_trading/modes/mode_config.py
@@ -93,12 +93,12 @@ def user_select_order_amount(trading_mode, inputs: dict, include_buy=True, inclu
     if include_buy:
         trading_mode.UI.user_input(
                 constants.CONFIG_BUY_ORDER_AMOUNT, common_enums.UserInputTypes.TEXT, "", inputs,
-                title=_get_order_amount_title("buy")
+                title=_get_order_amount_title("buy"), other_schema_values={"minLength": 0}
         )
     if include_sell:
         trading_mode.UI.user_input(
                 constants.CONFIG_SELL_ORDER_AMOUNT, common_enums.UserInputTypes.TEXT, "", inputs,
-                title=_get_order_amount_title("sell")
+                title=_get_order_amount_title("sell"), other_schema_values={"minLength": 0}
         )
 
 
