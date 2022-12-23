5133c6a35d2e45da93bba840787f5f133ae490db
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-12-23 15:57:34 +0100

    [TradingModes] generalize clear_simulated_orders_cache and clear_plotting_cache

--- octobot_trading/modes/modes_util.py
+++ octobot_trading/modes/modes_util.py
@@ -13,13 +13,31 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+import octobot_commons.databases as databases
 import octobot_tentacles_manager.api as tentacles_manager_api
 import octobot_trading.constants as constants
 import octobot_commons.constants as common_constants
+import octobot_trading.modes.script_keywords.basic_keywords as basic_keywords
 
 
 def get_required_candles_count(trading_mode_class, tentacles_setup_config):
     return tentacles_manager_api.get_tentacle_config(tentacles_setup_config, trading_mode_class).get(
         constants.CONFIG_CANDLES_HISTORY_SIZE_KEY,
         common_constants.DEFAULT_IGNORED_VALUE
+    )
+
+
+async def clear_simulated_orders_cache(trading_mode):
+    await basic_keywords.clear_orders_cache(
+        databases.RunDatabasesProvider.instance().get_orders_db(
+            trading_mode.bot_id, trading_mode.exchange_manager.exchange_name
+        )
+    )
+
+
+async def clear_plotting_cache(trading_mode):
+    await basic_keywords.clear_symbol_plot_cache(
+        databases.RunDatabasesProvider.instance().get_symbol_db(
+            trading_mode.bot_id, trading_mode.exchange_manager.exchange_name, trading_mode.symbol
+        )
     )
