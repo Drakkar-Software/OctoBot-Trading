ec2a046b7be4687591098ab6ad1339a15254defe
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-01-07 22:54:00 +0100

    [Ticker] create mini ticker, mark price and funding rate in channel

--- octobot_trading/exchange_data/ticker/channel/ticker_updater.py
+++ octobot_trading/exchange_data/ticker/channel/ticker_updater.py
@@ -68,9 +68,6 @@ class TickerUpdater(ticker_channel.TickerProducer):
             ticker: dict = await self.channel.exchange_manager.exchange.get_price_ticker(pair)
             if self._is_valid(ticker):
                 await self.push(pair, ticker)
-                await self.parse_mini_ticker(pair, ticker)
-                if self.channel.exchange_manager.is_future:
-                    await self.parse_future_data(pair, ticker)
             else:
                 self.logger.debug(f"Ignored incomplete ticker: {ticker}")
         except errors.FailedRequest as e:
@@ -92,40 +89,9 @@ class TickerUpdater(ticker_channel.TickerProducer):
         except KeyError:
             return False
 
-    def _cleanup_ticker_dict(self, ticker):
-        try:
-            ticker.pop("info")
-            ticker.pop("symbol")
-            ticker.pop("datetime")
-        except KeyError as e:
-            self.logger.error(f"Fail to cleanup ticker dict ({e})")
-        return ticker
-
     def _get_pairs_to_update(self):
         return self.channel.exchange_manager.exchange_config.traded_symbol_pairs + self._added_pairs
 
-    async def parse_mini_ticker(self, pair, ticker):
-        """
-        Mini ticker
-        """
-        try:
-            await exchanges_channel.get_chan(constants.MINI_TICKER_CHANNEL,
-                                             self.channel.exchange_manager.id).get_internal_producer(). \
-                push(pair, {
-                enums.ExchangeConstantsMiniTickerColumns.HIGH_PRICE.value:
-                    ticker[enums.ExchangeConstantsTickersColumns.HIGH.value],
-                enums.ExchangeConstantsMiniTickerColumns.LOW_PRICE.value:
-                    ticker[enums.ExchangeConstantsTickersColumns.LOW.value],
-                enums.ExchangeConstantsMiniTickerColumns.OPEN_PRICE.value:
-                    ticker[enums.ExchangeConstantsTickersColumns.OPEN.value],
-                enums.ExchangeConstantsMiniTickerColumns.CLOSE_PRICE.value:
-                    ticker[enums.ExchangeConstantsTickersColumns.CLOSE.value],
-                enums.ExchangeConstantsMiniTickerColumns.VOLUME.value:
-                    ticker[enums.ExchangeConstantsTickersColumns.BASE_VOLUME.value],
-                enums.ExchangeConstantsMiniTickerColumns.TIMESTAMP.value:
-                    ticker[enums.ExchangeConstantsTickersColumns.TIMESTAMP.value]})
-        except Exception as e:
-            self.logger.error(f"Failed to parse mini ticker : {e}")
 
     """
     Future data management
@@ -137,38 +103,6 @@ class TickerUpdater(ticker_channel.TickerProducer):
                        self.channel.exchange_manager.exchange.FUNDING_IN_TICKER
                        or self.channel.exchange_manager.exchange.MARK_PRICE_IN_TICKER
                )
-
-    async def parse_future_data(self, symbol: str, ticker: dict):
-        if self.channel.exchange_manager.exchange.MARK_PRICE_IN_TICKER:
-            await self.extract_mark_price(symbol, ticker)
-
-        if self.channel.exchange_manager.exchange.FUNDING_IN_TICKER:
-            await self.extract_funding_rate(symbol, ticker)
-
-    async def extract_mark_price(self, symbol: str, ticker: dict):
-        try:
-            ticker = self.channel.exchange_manager.exchange.parse_mark_price(ticker, from_ticker=True)
-            await exchanges_channel.get_chan(constants.MARK_PRICE_CHANNEL,
-                                             self.channel.exchange_manager.id).get_internal_producer(). \
-                push(symbol,
-                     decimal.Decimal(str(ticker[enums.ExchangeConstantsMarkPriceColumns.MARK_PRICE.value])))
-        except Exception as e:
-            self.logger.exception(e, True, f"Fail to update mark price from ticker : {e}")
-
-    async def extract_funding_rate(self, symbol: str, ticker: dict):
-        try:
-            ticker = self.channel.exchange_manager.exchange.parse_funding(ticker, from_ticker=True)
-            predicted_funding_rate = ticker.get(enums.ExchangeConstantsFundingColumns.PREDICTED_FUNDING_RATE.value,
-                                                constants.NaN)
-            await exchanges_channel.get_chan(constants.FUNDING_CHANNEL,
-                                             self.channel.exchange_manager.id).get_internal_producer(). \
-                push(symbol,
-                     decimal.Decimal(str(ticker[enums.ExchangeConstantsFundingColumns.FUNDING_RATE.value])),
-                     decimal.Decimal(str(predicted_funding_rate or constants.NaN)),
-                     ticker[enums.ExchangeConstantsFundingColumns.NEXT_FUNDING_TIME.value],
-                     ticker[enums.ExchangeConstantsFundingColumns.LAST_FUNDING_TIME.value])
-        except Exception as e:
-            self.logger.exception(e, True, f"Fail to update funding rate from ticker : {e}")
 
     async def modify(self, added_pairs=None, removed_pairs=None):
         if added_pairs:
@@ -194,10 +128,6 @@ class TickerUpdater(ticker_channel.TickerProducer):
         # there can be many ticker requests when a large number of currency is in a
         # portfolio, in this case, limit those requests
         self.refresh_time = self.TICKER_REFRESH_TIME * delay_multiplier
-
-    # async def config_callback(self, exchange, cryptocurrency, symbols, time_frames):
-    #     if symbols:
-    #         await self.modify(added_pairs=symbols)
 
     async def resume(self) -> None:
         await super().resume()
