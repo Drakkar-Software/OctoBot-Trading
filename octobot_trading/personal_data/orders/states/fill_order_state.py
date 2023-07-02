ffb0988f3e7eb3ed161e545a356b7f72fd68b183
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-07-02 18:02:05 +0200

    [Positions] fix position id rare issues

--- octobot_trading/personal_data/orders/states/fill_order_state.py
+++ octobot_trading/personal_data/orders/states/fill_order_state.py
@@ -106,7 +106,8 @@ class FillOrderState(order_state.OrderState):
                 async with self.order.exchange_manager.exchange_personal_data.portfolio_manager.portfolio.lock:
                     self.ensure_not_cleared(self.order)
                     await self.order.exchange_manager.exchange_personal_data.handle_portfolio_update_from_order(
-                        self.order)
+                        self.order
+                    )
 
                 # notify order filled
                 await self.order.exchange_manager.exchange_personal_data.handle_order_update_notification(
