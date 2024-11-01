2c72302ffc9f957d9b1e69da899a87e23c6ca213
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-11-01 10:21:27 +0100

    [Scripted] add DSL test

--- tests/modes/script_keywords/dsl/test_quantity.py
+++ tests/modes/script_keywords/dsl/test_quantity.py
@@ -72,7 +72,10 @@ def test_parse_quantity_types():
         script_keywords.QuantityType.ENTRY, decimal.Decimal("-0.11"))
 
     assert script_keywords.parse_quantity("p%") == (script_keywords.QuantityType.POSITION_PERCENT, None)
+    assert script_keywords.parse_quantity("%p") == (script_keywords.QuantityType.POSITION_PERCENT, None)
     assert script_keywords.parse_quantity("-0.11p%") == (
+        script_keywords.QuantityType.POSITION_PERCENT, decimal.Decimal("-0.11"))
+    assert script_keywords.parse_quantity("-0.11%p") == (
         script_keywords.QuantityType.POSITION_PERCENT, decimal.Decimal("-0.11"))
     assert script_keywords.parse_quantity("%p-0.11") == (
         script_keywords.QuantityType.POSITION_PERCENT, decimal.Decimal("-0.11"))
